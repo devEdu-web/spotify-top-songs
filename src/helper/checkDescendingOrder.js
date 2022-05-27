db3ed18aa0027c69c8ba26ddd9982d1a9974575a
Eduardo Oliveira <santos.edu3301@gmail.com>
2022-05-27 08:23:08 -0300

    readme update

--- src/helper/checkDescendingOrder.js
+++ src/helper/checkDescendingOrder.js
@@ -1,6 +1,5 @@
 function isDescending(array) {
   let descending = 'null';
-
   for (let i = 0; i < array.length; i++) {
     if (array[i + 1] == undefined) return descending;
     if (array[i] >= array[i + 1]) {
